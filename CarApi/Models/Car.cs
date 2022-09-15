3888ebda64094328bd53f4c74cf0445c36630f05
mabogdan <mabogdan@EN411340>
2022-09-15 09:09:24 +0300

    Add project files.

---
+++ CarApi/Models/Car.cs
@@ -0,0 +1,9 @@
+﻿namespace CarApi.Models
+{
+    public class Car
+    {
+        public int Id { get; set; }
+        public string? Name { get; set; }
+        public string? Color { get; set; }
+    }
+}
---
+++ CarApi/Models/CarContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+
+namespace CarApi.Models
+{ 
+    public class CarContext :DbContext 
+    {
+        public CarContext(DbContextOptions<CarContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Car> Cars { get; set; } = null!;
+    }
+}
